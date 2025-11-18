e9f26db3d8d567c7eae32e14dc4690a667cb287f
Felix Rosseeuw <felix.rosseeuw@nandou.polytechnique.fr>
2025-11-18 09:56:09 +0100

    part1

--- Lab4/part2/code_lab_community_detection.py
+++ Lab4/part2/code_lab_community_detection.py
@@ -12,23 +12,30 @@ from sklearn.cluster import KMeans
 
 
 ############## Task 3
-# Perform spectral clustering to partition graph G into k clusters
 def spectral_clustering(G, k):
+    M = nx.adjacency_matrix(G).todense()  # conversion en matrice dense
+    n = G.number_of_nodes()
+    D = np.zeros((n,n))
+    for i in range(n):
+        D[i,i] = np.sum(M[i,:])
     
-    ##################
-    # your code here #
-    ##################
+    L = np.identity(n) - np.linalg.inv(D) @ M
+    eigvals, eigvecs = np.linalg.eig(L)
     
-    
+    idx = np.argsort(eigvals)[:k]
+    U = np.real(eigvecs[:, idx])  
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    clustering = kmeans.fit_predict(U)
     
     return clustering
 
-
 ############## Task 4
 
-##################
-# your code here #
-##################
+G=nx.read_edgelist("/users/eleves-b/2022/felix.rosseeuw/LAB4-MVA/Lab4/datasets/CA-HepTh.txt")
+
+clustering=spectral_clustering(G,k=50)
+
+print(clustering[0:10])
 
 
 
@@ -36,25 +43,34 @@ def spectral_clustering(G, k):
 ############## Task 5
 # Compute modularity value from graph G based on clustering
 def modularity(G, clustering):
+    nodes = list(G.nodes())
+    index = {node: i for i, node in enumerate(nodes)}
+    k=np.max(clustering)+1
+    m=G.number_of_edges()
+    L=np.zeros(k)
+    U=np.zeros(k)
+    for (u,v) in G.edges() :
+        if clustering[index[u]]==clustering[index[v]] :
+            L[clustering[index[u]]]+=1
+    for i, node in enumerate(nodes):
+        U[clustering[i]] += G.degree(node)
+  
+    return(np.sum(L/m-U*U/(4*m**2)))
+
+
     
-    ##################
-    # your code here #
-    ##################
     
     
-    
-    
-    return modularity
 
 
 
 ############## Task 6
 
-##################
-# your code here #
-##################
+print(modularity(G, clustering))
 
+clustering=np.array([randint(0,50) for i in range (G.number_of_nodes())])
 
+print(modularity(G, clustering))
 
 
 
